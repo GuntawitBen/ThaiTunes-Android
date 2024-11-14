606fab37962f135599c87a819903b2796dbd15eb
GuntawitBen <132637794+GuntawitBen@users.noreply.github.com>
2024-11-15 00:00:48 +0700

    theme and app bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.egci428.egci428_poppic"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.egci428.egci428_poppic"
