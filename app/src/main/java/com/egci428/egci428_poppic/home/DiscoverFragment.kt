5c28c27a2abf708b37a23c5e4c826482dae60858
GuntawitBen <132637794+GuntawitBen@users.noreply.github.com>
2024-11-24 10:52:11 +0700

    revert

--- app/src/main/java/com/egci428/egci428_poppic/home/DiscoverFragment.kt
+++ app/src/main/java/com/egci428/egci428_poppic/home/DiscoverFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.egci428.egci428_poppic.ARG_PARAM1
-import com.egci428.egci428_poppic.ARG_PARAM2
 import com.egci428.egci428_poppic.R
 
 /**
